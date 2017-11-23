d8ab7e96fbdb92567d1f73b801c426ff82a78382
asbscsdses <779954817@qq.com>
2017-11-23 10:42:54 +0800

    Initial commit

---
+++ CmdGUI/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+
+namespace CmdGUI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Cmd Cmd { get; set; }
+        private String CommandListFileName { get { return "CmdList.txt"; }}
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadCommandList();
+
+            Cmd = new Cmd(ReadLineCallback);
+        }
+
+        private void LoadCommandList()
+        {
+            var configPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, CommandListFileName);
+
+            if (!File.Exists(configPath)) return;
+
+            var commands = File.ReadAllLines(configPath);
+
+            foreach (var cmd in commands.Where(s => !CbCmd.Items.Contains(s)))
+            {
+                CbCmd.Items.Add(cmd);
+            }
+        }
+
+        private void ReadLineCallback(String line)
+        {
+            Dispatcher.Invoke(new Action(() => TbResult.AppendText(line + Environment.NewLine)));
+        }
+
+        private void OnExcuteClick(object sender, RoutedEventArgs e)
+        {
+            TbResult.Clear();
+            Cmd.RunCmdAsync(CbCmd.Text.Trim());
+        }
+    }
+}
